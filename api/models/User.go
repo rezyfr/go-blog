628e5bb638798ae839a3916e696d7408697a4fb7
Fidriyanto Rizkillah <fidriyanto.rizkillah@amartha.com>
2021-07-03 12:31:32 +0700

    fix wrong variable

--- api/models/User.go
+++ api/models/User.go
@@ -24,7 +24,7 @@ func Hash(password string) ([]byte, error) {
 	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 }
 
-func verifyPassword(hashedPassword, password string) error {
+func VerifyPassword(hashedPassword, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 }
 
